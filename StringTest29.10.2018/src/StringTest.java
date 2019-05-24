6cc3feac2472db9f606e236bc0f70d558edde932
Venkat <ginnarapusap@gmail.com>
2019-05-24 17:02:35 +0100

    MyProject code pushed

---
+++ StringTest29.10.2018/src/StringTest.java
@@ -0,0 +1,26 @@
+public class StringTest {
+
+	public static void main(String[] args) {
+		String s = new String("VENKAT Ginnarapu Aarvi Rao");
+		
+		String rev = "";
+		
+		for (int i=s.length()-1; i>=0; i--){
+			rev = rev + s.charAt(i);
+		}
+		System.out.println(rev);
+
+	}
+
+	public static void main1(String[] args) {
+
+		String s = new String("VENKAT Ginnarapu Aarvi Rao");
+
+		String[] values = s.split(" ");
+
+		for (int i = values.length - 1; i >= 0; i--) {
+			System.out.print(values[i] + " ");
+		}
+	}
+
+}
